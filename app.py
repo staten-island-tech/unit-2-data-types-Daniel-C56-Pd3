c56e91d363f81897f39f346471d79eca6be8daf1
Daniel-C56-Pd3 <danield494@nycstudents.net>
2024-09-17 10:14:10 -0400

    app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+# values = [1,2.23,5,7,2,30,15]
+# print(values)
+# for i in values:
+#     print(values[0])
+#     print(values[6])
+
+# x = "this is a thing"
+# y= x.split( )
+# z = y[1]
+# print(y)
+# print(z)
+
+
+#1 Challenge
+# you can type words in to the text box and i will tell yo then amount of word there aree by the amount of spaces
+
+# x = input('Type Here')
+# y= x.split( )
+# print(len(y))
+
+#2
+
+x = input('walk', 'run')
+print(x)
